110921afa897d9c3f60e06cce5c29f77941a0d5a
Jose Manuel Quintero Rodriguez <joquintero@sistecredito.com>
2024-04-04 16:02:34 -0500

    .

--- Application/DTOs/Common/Product.cs
+++ Application/DTOs/Common/Product.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Services.Compound
+namespace Application.DTOs.Common
 {
-    public class ProductoService
+    public  class Product
     {
+
     }
 }
