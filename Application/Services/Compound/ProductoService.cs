41683981bdeaa990b16a182083a275bb78d1b3ca
Jose Manuel Quintero Rodriguez <joquintero@sistecredito.com>
2024-04-04 15:46:41 -0500

    commit se movio el archivo del servicio a carpeta independiente de la interfaz

--- Application/Services/Compound/ProductoService.cs
+++ Application/Services/Compound/ProductoService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Interfaces.Services
+namespace Application.Services.Compound
 {
-    public  class ProductoService
+    public class ProductoService
     {
     }
 }
